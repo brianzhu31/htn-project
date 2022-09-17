3a984ffaa8c58fd903a1931706975be9b20814b8
br0mabs <90655166+br0mabs@users.noreply.github.com>
2022-09-17 19:44:44 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
 import React, { useState } from 'react'
-import $ from 'jquery';
 import Home from './pages/Home';
 
 function App() {
+
+  const teams = [
+    'Atlanta Hawks',
+    'Boston Celtics',
+    'Brooklyn Nets',
+    'Charlotte Hornets',
+    'Chicago Bulls',
+    'Cleveland Cavaliers',
+    'Dallas Mavericks',
+    'Denver Nuggets',
+    'Detroit Pistons',
+    'Golden State Warriors',
+    'Houston Rockets',
+    'Indiana Pacers',
+    'LA Clippers',
+    'LA Lakers',
+    'Memphis Grizzlies',
+    'Miami Heat',
+    'Milwaukee Bucks',
+    'Minnesota Timberwolves',
+    'New Orleans Hornets',
+    'New York Knicks',
+    'Oklahoma City Thunder',
+    'Orlando Magic',
+    'Philadelphia Sixers',
+    'Phoenix Suns',
+    'Portland Trail Blazers',
+    'Sacramento Kings',
+    'San Antonio Spurs',
+    'Toronto Raptors',
+    'Utah Jazz',
+    'Washington Wizards'
+  ]
   
+  const teamtoid = new Map();
+  for (let i = 0; i < 30; ++i) {
+      teamtoid.set(teams[i], i + 1);
+  }
+
   return (
     <>
       <Home />
