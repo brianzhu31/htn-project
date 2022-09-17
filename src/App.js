956dbf18b978afbd2f6c5b572e09c63a9c961764
Brian Zhu <60515336+brianzhu31@users.noreply.github.com>
2022-09-17 12:47:00 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import NewComponent from './newComponent';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React, { useState } from 'react'
+import $ from 'jquery';
 
 function App() {
+
+  function loadTeams() {
+    $.getJSON('https://www.balldontlie.io/api/v1/teams', function(data) {
+      console.log(data)
+    })
+  }
+
   return (
     <>
-      <div>Hello</div>
-      <NewComponent />
+      <button onClick={loadTeams}>click</button>
     </>
   );
 }
